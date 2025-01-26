35b99cd7e4e36a1f1a6514e7f58adb85a580987a
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2025-01-26 04:54:49 -0800

    Add bubble spawn interval to Bubble Gun

--- Assets/The Surfacing/Scripts/Weapons/Bubble Gun/BubbleGun.cs
+++ Assets/The Surfacing/Scripts/Weapons/Bubble Gun/BubbleGun.cs
@@ -12,6 +12,12 @@ public class BubbleGun : MonoBehaviour
     [field: SerializeField] public float BubbleTravelTime { get; set; }
     [field: SerializeField] public int MaxBubbleInstances { get; set; } = 5;
     
+    [Tooltip("If false, bubbles cannot be created until all player-created bubbles have popped.")]
+    [field: SerializeField] public bool ConsecutiveBubbleSpawning { get; set; }
+    
+    [field: SerializeField] private float BubbleSpawnInterval { get; set; } = 1f;
+    
+    
     [Header("Bubble Spawn Settings")]
     [field: SerializeField] private float BubbleLifespan { get; set; } = 3;
 
@@ -25,15 +31,18 @@ public class BubbleGun : MonoBehaviour
     [field: SerializeField] public EventReference _gunShoot;
     
     public List<Bubble> _bubbles;
+    private bool _canShoot;
+    private float _shootTimer;
     
     private void Awake()
     {
         _bubbles = new List<Bubble>();
+        _canShoot = true;
+        _shootTimer = 0;
     }
     
     private void Update()
     {
-        Debug.Log($"Bubble instances: {_bubbles.Count}");
         if (_bubbles.Count - 1 >= MaxBubbleInstances)
         {
             if (_bubbles[0] != null)
@@ -46,7 +55,16 @@ public class BubbleGun : MonoBehaviour
 
     public void CreateBubble()
     {
-        if (_bubbles.Count >= MaxBubbleInstances) return;
+        if (ConsecutiveBubbleSpawning)
+        {
+            if (_bubbles.Count >= MaxBubbleInstances) return;
+        }
+        else
+        {
+            if (_bubbles.Count >= MaxBubbleInstances) StartCoroutine(WaitForBubbles());
+        }
+        if (!_canShoot) return;
+        
         Vector3 spawnPosition = (transform.forward * BubbleSpawnDistance) + (transform.position + Vector3.up);
         Vector3 destination = (transform.forward * (BubbleSpawnDistance + BubbleTravelDistance)) + (transform.position + Vector3.up);
 
@@ -62,6 +80,13 @@ public class BubbleGun : MonoBehaviour
             _bubbles.Add(bubbleComponent);
             bubbleComponent.PushBubble(destination, Time.deltaTime * BubbleTravelTime);
         }
+    }
+
+    private IEnumerator WaitForBubbles()
+    {
+        _canShoot = false;
+        yield return new WaitForSeconds(BubbleSpawnInterval);
+        _canShoot = true;
     }
     
     private void OnDrawGizmosSelected()
