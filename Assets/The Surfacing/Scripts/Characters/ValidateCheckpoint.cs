262944111c22395b9591f67a736045e33184502f
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2025-01-26 06:08:44 -0800

    Create checkpoint validation script

---
+++ Assets/The Surfacing/Scripts/Characters/ValidateCheckpoint.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ValidateCheckpoint : MonoBehaviour
+{
+    public Dictionary<int, Vector3> checkpoints = new Dictionary<int, Vector3>();
+    
+    
+}
--- Assets/The Surfacing/Scripts/Data/CheckpointData.cs
+++ Assets/The Surfacing/Scripts/Data/CheckpointData.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "CheckpointData", menuName = "Scriptables/Checkpoint Data")]
 public class CheckpointData : ScriptableObject
 {
     public int CheckpointID;
