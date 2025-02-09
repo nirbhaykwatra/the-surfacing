88bf3d530f5429307c7db7501eab3e4730318b6f
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2025-02-08 22:11:25 -0800

    Create unstable/breakable platform logic

---
+++ Assets/1 - The Surfacing/Scripts/Environment/UnstablePlatform.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+public class UnstablePlatform : MonoBehaviour
+{
+    public float BreakDelay = 0.5f;
+    public float RespawnTime = 5f;
+
+    private bool _break = false;
+    private bool _broken = false;
+    private float _timer;
+    
+    private MeshRenderer _meshRenderer;
+    private Collider _collider;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        if (_break)
+        {
+            _meshRenderer.material.color = Color.red;
+            _timer += Time.deltaTime;
+            if (_timer >= BreakDelay)
+            {
+                _meshRenderer.enabled = false;
+                _collider.enabled = false;
+                _broken = true;
+                _timer = 0;
+                _break = false;
+            }
+        }
+
+        if (_broken)
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= RespawnTime)
+            {
+                Respawn();
+                _broken = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent(out CharacterMovement3D character))
+        {
+            _break = true;
+        }
+    }
+
+    private void Respawn()
+    {
+        _meshRenderer.enabled = true;
+        _collider.enabled = true;
+    }
+}
